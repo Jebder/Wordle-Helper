8473b938aef1827053be74b1637cf22f8473f9d0
Jebder <zdbrown2343@gmail.com>
2023-05-26 19:02:39 -0400

    Add words.js dictionary file

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-let wordList = [];
+let wordArray = [];
+
+fetch('https://jebder.github.io/Wordle-Solver/sgb-words.txt')
+  .then(response => response.text())
+  .then(data => {
+    // Split the text by newlines to get individual words
+    wordArray = data.split('\n');
+    
+    // Do something with the wordArray here
+    console.log(wordArray);
+  })
+  .catch(error => {
+    console.log('Error fetching the word list:', error);
+  });
+
+  
