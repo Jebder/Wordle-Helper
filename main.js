e631d1fec6341d2a02c086456fa0e76712c61f35
Jebder <zdbrown2343@gmail.com>
2023-05-27 02:44:32 -0400

    Add button logic and remove old enter button.

--- main.js
+++ main.js
@@ -1,16 +1,54 @@
-let wordArray = [];
+let wordDict = {
+    firstWord: null,
+    secondWord: null, 
+    thirdWord: null, 
+    fourthWord: null, 
+    fifthWord: null, 
+    lastWord: null
+};
 
-fetch('https://jebder.github.io/Wordle-Solver/sgb-words.txt')
-  .then(response => response.text())
-  .then(data => {
-    // Split the text by newlines to get individual words
-    wordArray = data.split('\n');
-    
-    // Do something with the wordArray here
-    console.log(wordArray);
-  })
-  .catch(error => {
-    console.log('Error fetching the word list:', error);
-  });
+let letters = [null, null, null, null, null]; 
 
-  
+
+function combineLetters(letters) {
+    let word = "";
+    for (let i = 0; i < letters.length; i++) {
+      word = word + letters[i];
+    }
+    return word;
+  };
+
+function populateWord(input) {
+    if (input === 'BACKSPACE'){
+        // DO BACKSPACE STUFF
+    } else if (input === 'ENTER'){
+        for (let word in wordDict) {
+            if (wordDict[word] === null){
+                wordDict[word] = combineLetters(letters);
+                letters = [null, null, null, null, null]; // reset letters variable
+                break;
+            };
+        };
+    } else{
+        for (let i = 0;i < 5; i++){
+            if (letters[i] === null) {
+                letters[i] = input;
+                console.log(letters);
+                break;
+            };
+        };
+    };
+};
+
+function initializeKeyboard(){
+    let keys = document.querySelectorAll('.key');
+    keys.forEach((key) => {
+        key.addEventListener('click', ()=>{
+            let keyValue = key.innerHTML;
+            populateWord(keyValue);
+            console.log(wordDict);
+        })
+    });
+};
+
+initializeKeyboard();
---
+++ solverLogic.js
@@ -0,0 +1,1 @@
+import wordArray from '/home/jeb/repos/Wordle-Solver/words.js';
