8fe11bf7187690cc020da97f8f306d03c5e2db10
Jebder <zdbrown2343@gmail.com>
2023-05-28 15:35:40 -0400

    Test.

--- solverLogic.js
+++ solverLogic.js
@@ -2,5 +2,7 @@ import wordArray from '/home/jeb/repos/Wordle-Solver/words.js';
 
 const wordList = wordArray; //maybe redundant but more readable.
 
-
-
+function colorDeclaration(){
+    alert('Click the letter to change it/s color.');
+    
+};
