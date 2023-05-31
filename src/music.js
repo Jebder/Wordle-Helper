33ef93ed84d4b5489b69cf9c804b50cc892294a7
Gitgotit <111936513+Gitgotit@users.noreply.github.com>
2023-05-30 23:18:49 -0400

    Added play/pause button for music.

--- src/music.js
+++ src/music.js
@@ -5,3 +5,13 @@ function myFunction() {
     audio.muted = !audio.muted;
     btn.innerHTML = audio.muted ? "Unmute":'Mute'
 }
+
+function playAudio() {
+  var audio = document.getElementById("myAudio");
+  audio.play();
+}
+
+function pauseAudio() {
+  var audio = document.getElementById("myAudio");
+  audio.pause();
+}
