8db5d2b40ad4df30a53a9d28eff8775e850927fd
Jebder <zdbrown2343@gmail.com>
2023-06-02 03:20:00 -0400

    Progress on logic. Green and White work only.

--- src/solverLogic.js
+++ src/solverLogic.js
@@ -1,6 +1,6 @@
 let newWordList = [];
 function logic(){
-  for (let i = 0; i < 2; i++) {
+  for (let i = 0; i < 5; i++) {
     console.log(translatedColorArray)
     console.log(i);
     console.log(letters)
@@ -10,17 +10,20 @@ function logic(){
       case 'r':
           rightLetterElim(letters[i],i);
           console.log('rightElim');
+          console.log(newWordList);
           break;
       case 'w': 
           wrongLetterElim(letters[i]);
           console.log('wrongElim');
+          console.log(newWordList);
           break;
       case 'wp':
           wrongPlaceElim(letters[i],i);
           console.log('WPElim');
+          console.log(newWordList);
           break;
           
-    }
+    };
   };
 
   // function to update dom with new pruned word list
@@ -31,60 +34,109 @@ function logic(){
 };
 
 // first?
-function rightLetterElim(refLetter,pos){
-  // set word list to the already pruned list if it exists. Should be faster?
-  if (newWordList.length !== 0){
+function rightLetterElim(refLetter, pos) {
+  let tempWordList = [];
+
+  if (newWordList.length !== 0) {
     for (let word of newWordList) {
       if (word[pos] === refLetter.toLowerCase()) {
-        newWordList.push(word);
+        tempWordList.push(word);
       };
     };
-  }else{
-    for (let word of allWords) {
-      if (word[pos] === refLetter.toLowerCase()) {
-        newWordList.push(word);
-      };
-    };
-  };
-  // prune list
-};
-//second?
-function wrongLetterElim(refLetter){
-  // set word list to the already pruned list if it exists. Should be faster?
-  if (newWordList.length !== 0){
-    for (let word of newWordList) {
-      let containsRefLetter = false;
-      
-      for (let letter of word) {
-        if (letter === refLetter) {
-          containsRefLetter = true;
-          break;
-        }
-      }
-      
-      if (!containsRefLetter) {
-        newWordList.push(word);
-      }
-    }
+    newWordList = tempWordList; // Update newWordList with the new filtered list
+    console.log('right letter. 1');
   } else {
     for (let word of allWords) {
+      if (word[pos] === refLetter.toLowerCase()) {
+        tempWordList.push(word);
+      };
+    };
+    newWordList = tempWordList; // Update newWordList with the new filtered list
+    console.log('right letter. 2');
+  };
+};
+//second?
+function wrongLetterElim(refLetter) {
+  let tempWordList = [];
+
+  if (newWordList.length !== 0) {
+    for (let word of newWordList) {
       let containsRefLetter = false;
-      
+
       for (let letter of word) {
         if (letter === refLetter) {
           containsRefLetter = true;
           break;
         };
       };
-      
+
       if (!containsRefLetter) {
-        newWordList.push(word);
+        tempWordList.push(word);
       };
     };
+    newWordList = tempWordList; // Update newWordList with the new filtered list
+    console.log('wrong letter. 1');
+  } else {
+    for (let word of allWords) {
+      let containsRefLetter = false;
+
+      for (let letter of word) {
+        if (letter === refLetter) {
+          containsRefLetter = true;
+          break;
+        };
+      };
+
+      if (containsRefLetter) {
+        tempWordList.push(word);
+      };
+    };
+    newWordList = tempWordList; // Update newWordList with the new filtered list
+    console.log('wrong letter. 2');
   };
 };
 
-function wrongPlaceElim(refLetter, pos){
-  console.log('it worked')
+function wrongPlaceElim(refLetter, pos) {
+  let tempWordList = [];
+
+  if (newWordList.length !== 0) {
+    for (let word of newWordList) {
+      let containsRefLetter = false;
+
+      if (word[pos] !== refLetter) {
+        for (let letter of word) {
+          if (letter === refLetter) {
+            containsRefLetter = true;
+            break;
+          };
+        };
+        if (containsRefLetter) {
+          tempWordList.push(word);
+        }; 
+      };
+    };
+    newWordList = tempWordList; // Update newWordList with the new filtered list
+    console.log('WP. 1');
+  }
+   else {
+    for (let word of allWords) {
+      let containsRefLetter = false;
+
+      if (word[pos] !== refLetter) {
+        for (let letter of word) {
+          if (letter === refLetter) {
+            containsRefLetter = true;
+            break;
+          };
+        };
+        if (containsRefLetter) {
+          tempWordList.push(word);
+        };
+      };
+    };
+    newWordList = tempWordList; // Update newWordList with the new filtered list
+    console.log('WP. 2');
+  };
 };
+
 
